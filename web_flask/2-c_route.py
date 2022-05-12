187bacb443737fca221fd18259a7b53cba577bcc
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-05-12 11:30:46 +0200

    Python script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,7 +14,7 @@ def hbnb():
 
 @app.route('/c/<text>',strict_slashes = False)
 def c(text):
-    """ Print HBNB"""
+    """ Print C and text"""
     return 'C {}'.format(text.replace("_"," "))
 
 if __name__ == '__main__':
