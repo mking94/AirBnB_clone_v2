24bf4cc3711546868c3c91cd44a6fab46988fd93
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-05-12 11:27:37 +0200

    Python script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Python script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+@app.route('/',strict_slashes = False)
+def index():
+    """ Print Hello HBNB"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb',strict_slashes = False)
+def hbnb():
+    """ Print HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<text>',strict_slashes = False)
+def c(text):
+    """ Print HBNB"""
+    return 'C {}'.format(text.replace("_"," "))
+
+if __name__ == '__main__':
+      app.run(host="0.0.0.0", port=5000)
