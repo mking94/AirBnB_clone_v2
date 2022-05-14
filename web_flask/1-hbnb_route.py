f5f2f4a38e49cf704fb5befbc21ec2cf62323cd5
mking94 <dadimaher94@gmail.com>
2022-05-14 02:55:43 -0700

    Python script that starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,15 +2,19 @@
 """ Python script that starts a Flask web application """
 from flask import Flask
 app = Flask(__name__)
-@app.route('/',strict_slashes = False)
+
+
+@app.route('/', strict_slashes=False)
 def index():
     """ Print Hello HBNB"""
     return 'Hello HBNB!'
 
-@app.hbnb('/hbnb',strict_slashes = False)
+
+@app.hbnb('/hbnb', strict_slashes=False)
 def hbnb():
     """ Print HBNB"""
     return 'HBNB'
 
+
 if __name__ == '__main__':
-      app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
