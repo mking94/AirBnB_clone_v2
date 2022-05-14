4f8c9b30d51fbd04526288c100f784fb03bd3f2c
mking94 <dadimaher94@gmail.com>
2022-05-14 03:18:33 -0700

    Python script that starts a Flask web application

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,44 +2,54 @@
 """ Python script that starts a Flask web application """
 from flask import Flask, abort, render_template
 app = Flask(__name__)
-@app.route('/',strict_slashes = False)
+
+
+@app.route('/', strict_slashes=False)
 def index():
     """ Print Hello HBNB"""
     return 'Hello HBNB!'
 
-@app.route('/hbnb',strict_slashes = False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Print HBNB"""
     return 'HBNB'
 
-@app.route('/c/<text>',strict_slashes = False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ Print C and text"""
-    return 'C {}'.format(text.replace("_"," "))
+    return 'C {}'.format(text.replace("_", " "))
 
-@app.route('/python',strict_slashes = False)
+
+@app.route('/python', strict_slashes=False)
 def python_default():
     """ Print Python is cool """
     return 'Python is cool'
 
-@app.route('/python/<text>',strict_slashes = False)
+
+@app.route('/python/<text>', strict_slashes=False)
 def python(text):
     """ Print Python """
-    return 'Python {}'.format(text.replace("_"," "))
+    return 'Python {}'.format(text.replace("_", " "))
 
-@app.route('/number/<n>',strict_slashes = False)
+
+@app.route('/number/<n>', strict_slashes=False)
 def num(n):
     """ Print number or redirect to 404 error """
     if(n.isnumeric()):
         return '{} is a number'.format(n)
     abort(404)
 
-@app.route('/number_template/<n>',strict_slashes = False)
+
+@app.route('/number_template/<n>', strict_slashes=False)
 def num_temp(n):
-    """ Print number in html code 
+    """ Print number in html code
     or redirect to 404 error """
     if(n.isnumeric()):
         return render_template("5-number.html", value=n)
     abort(404)
+
+
 if __name__ == '__main__':
-      app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
