d54ccbd8c5c8a4cd982f3e4153a348a209dd762c
mking94 <dadimaher94@gmail.com>
2022-05-15 04:43:43 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,7 @@ class FileStorage:
                 if isinstance(v, cls):
                     cls_dict[k] = v
             return cls_dict
-        return self.__objects    
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -35,12 +35,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
     def delete(self, obj=None):
         """Delete a given object from __objects, if it exists."""
         try:
             del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
-            pass            
+            pass
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -53,18 +54,19 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
     def close(self):
         """Call the reload method."""
         self.reload()
